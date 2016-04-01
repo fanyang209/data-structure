c72c1c395aad3d9f96bf52ccc62f3cd284048fdc
fan <fanyang209@gmail.com>
2016-04-01 08:48:11 -0700

    medium

---
+++ Array/Maximum Product Subarray.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public int maxProduct(int[] nums) {
+        if(nums == null || nums.length == 0) return 0;
+        
+        int maxProduct = nums[0];
+        int maxLocal = nums[0];
+        int minLocal = nums[0];
+        
+        for(int i = 1; i < nums.length; i++){
+            int temp = maxLocal;
+            maxLocal = Math.max(Math.max(nums[i] * maxLocal, nums[i]), nums[i] * minLocal);
+            minLocal = Math.min(Math.min(nums[i] * temp, nums[i]), nums[i] * minLocal);
+            maxProduct = Math.max(maxProduct, maxLocal);
+        }
+        return maxProduct;
+    }
+}
