cd6772e5fa019ff5bc3486fa88c652a2fc923bd3
fan <fanyang209@gmail.com>
2016-04-25 09:12:13 -0700

    medium

---
+++ Backtracking/Word Search.java
@@ -0,0 +1,32 @@
+public class Solution {
+    public boolean exist(char[][] board, String word) {
+        int m = board.length;
+        int n = board[0].length;
+        boolean[][] visited = new boolean[m][n];
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(dfs(board, word, 0, i, j, visited))
+                    return true;
+            }
+        }
+        return false;
+    }
+    
+    public boolean dfs(char[][] board, String word, int index, int i, int j, boolean[][] visited){
+        if(index == word.length())
+            return true;
+        if(i < 0 || j < 0 || i >= board.length || j >= board[0].length)
+            return false;
+        if(visited[i][j])
+            return false;
+        if(board[i][j] != word.charAt(index))
+            return false;
+        visited[i][j] = true;
+        boolean res = dfs(board, word, index + 1, i - 1, j, visited)
+                    ||dfs(board, word, index + 1, i + 1, j, visited)
+                    ||dfs(board, word, index + 1, i, j + 1, visited)
+                    ||dfs(board, word, index + 1, i, j - 1, visited);
+        visited[i][j] = false;
+        return res;
+    }
+}
