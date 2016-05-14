8080b7226a38f1a35de40b8919e4a6fb3471af48
fan <fanyang209@gmail.com>
2016-05-14 09:41:17 -0700

    hard

---
+++ Math/Self Crossing.java
@@ -0,0 +1,16 @@
+// http://www.voidcn.com/blog/zdavb/article/p-5749387.html
+// http://www.cnblogs.com/grandyang/p/5216856.html
+public class Solution {
+    public boolean isSelfCrossing(int[] x) {
+        int length = x.length;
+        for(int i = 3; i < length; i++){
+            if(i >= 3 && x[i] >= x[i - 2] && x[i - 1] <= x[i - 3])
+                return true;
+            if(i >= 4 && x[i]+x[i-4] == x[i-2] && x[i-1] == x[i-3])
+                return true;
+            if(i >= 5 && x[i]+x[i-4] >= x[i-2] && x[i-1] + x[i-5] >= x[i-3] && x[i-4]<=x[i-2] && x[i-1] <= x[i-3])
+                return true;
+        }
+        return false;
+        }
+    }
