244dbbabd3c3654293ebcc8732e721f6d2aa9520
fan <fanyang209@gmail.com>
2016-05-23 09:10:41 -0700

    easy

---
+++ Binary Search/last occurrence.java
@@ -0,0 +1,25 @@
+public int lastOccur ( int[ ] arr, int target ) {
+	if ( arr == null || arr.length == 0 ) {
+	return -1 ;
+}
+int left = 0 ;
+int right = arr.length - 1 ;
+while ( left < right - 1) {
+	int mid = left + ( right - left ) / 2 ;
+	if ( arr[ mid ] == target ) {
+	left = mid ; // keep checking to left
+} else if ( arr[ mid ] < target ) {
+	left = mid ;
+} else {
+	right = mid ;
+}
+}
+if ( arr[ left ] == target ) {
+	return left ;
+}
+if ( arr[ right] == target ) {
+	return right;
+}
+return -1;
+
+}
