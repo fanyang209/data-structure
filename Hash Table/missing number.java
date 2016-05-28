58c70b25bfe409ae0a36258635149970926b5729
fan <fanyang209@gmail.com>
2016-05-28 09:38:14 -0700

    easy

---
+++ Hash Table/missing number.java
@@ -0,0 +1,13 @@
+	public int missingNumber( int[ ] array ) {
+		int n = array.length + 1 ;
+		HashSet<Integer> set = new HashSet<Integer>() ;
+		for( int number : array ) {
+			set.add( number ) ;
+		}
+		for( int i = 1; i < n ; i++ ) {
+			if( !set.contains(i) ) {
+				return i ;
+			}
+		}
+		return n ;
+	}
