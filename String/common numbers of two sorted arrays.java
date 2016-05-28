231faca5f5723a21ffd5ed92007e8f59cd4b9bb6
fan <fanyang209@gmail.com>
2016-05-28 09:39:40 -0700

    easy

---
+++ String/common numbers of two sorted arrays.java
@@ -0,0 +1,17 @@
+public List<Integer> commonI( int[ ] a, int[ ] b ) {
+		List<Integer> common = new ArrayList<Integer>() ;
+		int i = 0 ;
+		int j = 0 ;
+		while( i < a.length && j < b.length ) {
+			if( a[ i ] == b[ j ] ) {
+				common.add( a[ i ] ) ;
+				i++;
+				j++;
+			} else if ( a[ i ] < b[ j ] ) {
+				i++;
+			} else {
+				j++;
+			}
+		}
+		return common; 
+	}
