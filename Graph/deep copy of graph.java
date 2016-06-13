56938d65bf58433c9587efb49c71474c39b02387
fan <fanyang209@gmail.com>
2016-06-13 09:31:14 -0700

    easy

---
+++ Graph/deep copy of graph.java
@@ -0,0 +1,24 @@
+public List<GraphNode> copy( List<GraphNode> graph ) {
+	if( graph == null ) {
+	return null ;
+}
+HashMap<GraphNode, GraphNode> map = new HashMap<GraphNode, GraphNode>() ;
+for( GraphNode node : graph ) {
+	if( !map.containsKey( node ) ) {
+	map.put( node, new GraphNode( node.key ) ) ;
+	DFS( node, map ) ;
+}
+}
+return new ArrayList<GraphNode>( map.values() ) ;
+}
+
+public void DFS( GraphNode seed, HashMap<GraphNode, GraphNode>     map ) {
+	GraphNode copy = map.get( seed ) ;
+	for( GraphNode nei : seed.neighbors ) {
+		if( !map.containsKey( nei ) ) {
+map.put( nei, new GraphNode( nei.key ) ) ;
+DFS( nei, map ) ;
+}
+copy.neighbors.add( map.get( nei ) ) ; 
+}
+}
