2fad37f3bb8fd895326cf2b09f073b0f73f2eab2
Fan Yang <fanyang209@gmail.com>
2016-08-02 08:39:44 -0700

    medium

---
+++ Tree/Binary Tree Right Side View.java
@@ -0,0 +1,22 @@
+public class Solution {
+
+    int maxdepth = 0;
+    List<Integer> res;
+
+    public List<Integer> rightSideView(TreeNode root) {
+        res = new LinkedList<Integer>();
+        if(root!=null) helper(root,1);
+        return res;
+    }
+
+    private void helper(TreeNode root, int depth){
+        if(depth>maxdepth){
+            maxdepth = depth;
+            res.add(root.val);
+        }
+        if(root.right!=null) helper(root.right, depth+1);
+        if(root.left!=null) helper(root.left, depth+1);
+    }
+
+
+}
